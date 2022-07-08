b1f59ab9f08ad03310f902bd8710ef75ecfe266d
ansis-m <78793148+ansis-m@users.noreply.github.com>
2022-07-08 16:35:45 +0300

    Create RowAndColumn.java

---
+++ RowAndColumn.java
@@ -0,0 +1,28 @@
+package cinema;
+
+public class RowAndColumn {
+
+    int row;
+    int column;
+
+    RowAndColumn(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
